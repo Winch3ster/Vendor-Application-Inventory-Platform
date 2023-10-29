5e713a703a689acec67768dbb6d6910d6db49767
Cheong Yu Won <103837822+yuwon6881@users.noreply.github.com>
2023-10-29 12:23:49 +0800

    -Basic Login View with Basic backend implementation, future enhancement will be needed after database creation.
    -Minor tweaks to the Layout Page

---
+++ Vendor Application Inventory Platform/Controllers/AccessController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Vendor_Application_Inventory_Platform.Models;
+
+namespace Vendor_Application_Inventory_Platform.Controllers;
+
+public class AccessController : Controller
+{
+    // GET
+    public IActionResult Login()
+    {
+        ClaimsPrincipal claimUser = HttpContext.User;
+        if (claimUser.Identity.IsAuthenticated)
+            return RedirectToAction("Index", "Employee");
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Login(VMLogin modelLogin)
+    {
+        if (modelLogin is { email: "user@example.com", password: "123" })
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, modelLogin.email),
+                new Claim("OtherProperties", "Example Role")
+            };
+
+            ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            AuthenticationProperties properties = new AuthenticationProperties()
+            {
+                AllowRefresh = true,
+                IsPersistent = modelLogin.keepLoggedIn
+            };
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity), properties);
+            return RedirectToAction("Index", "Employee");
+        }
+
+
+
+        ViewData["ValidateMessage"] = "User not found";
+        return View();
+    }
+    
+    
+}
--- Vendor Application Inventory Platform/Controllers/EmployeeController.cs
+++ Vendor Application Inventory Platform/Controllers/EmployeeController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Vendor_Application_Inventory_Platform.Controllers
 {
+    [Authorize]
     public class EmployeeController : Controller
     {
         public IActionResult Index()
@@ -14,6 +18,11 @@ namespace Vendor_Application_Inventory_Platform.Controllers
             return View();
         }
 
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Access");
+        }
 
     }
 }
