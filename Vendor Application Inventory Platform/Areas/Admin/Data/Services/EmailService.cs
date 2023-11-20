e8da1d36752cb3522750e9a72ad06db5ce46bdd9
Kingston Lee <kingstonlee96@gmail.com>
2023-11-21 01:36:06 +0800

    Updated Email Services

--- Vendor Application Inventory Platform/Areas/Admin/Data/Services/EmailService.cs
+++ Vendor Application Inventory Platform/Areas/Admin/Data/Services/EmailService.cs
@@ -4,54 +4,62 @@ using MailKit.Net.Smtp;
 
 namespace Vendor_Application_Inventory_Platform.Areas.Admin.Data.Services
 {
-    // EmailService.cs
-    // EmailService.cs
+    //The user will receive email of notification for any changes made to the system's data like create, update and delete
     public class EmailService
     {
-        private readonly SmtpClient _smtpClient;
+        private readonly SmtpClient _smtpClient; //Get the client services
+
 
         public EmailService(IConfiguration configuration)
         {
+            //Get the email services from the appsetting.json
             var emailSettings = configuration.GetSection("EmailSettings");
 
-            _smtpClient = new SmtpClient();
+            _smtpClient = new SmtpClient(); //Intantiate a new client object
 
-            // Connect to the SMTP server
+            // Connect to the SMTP server, gmail smtp server is used (as specified in appsetting.json)
             _smtpClient.Connect(emailSettings["SmtpServer"], int.Parse(emailSettings["Port"]), useSsl: true);
 
-            // Authenticate with the server
+            // Authenticate the user with the specified server
             _smtpClient.Authenticate(emailSettings["UserName"], emailSettings["Password"]);
         }
+        
 
-        public void SendEmail(string to, string subject, string body)
+        //Send mail to user
+        public void SendEmail(string toEmail, string subject, string messageToSend)
         {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Vendor Application Inventory Platform", "JohnWilliamCitiSoft@gmail.com"));
+            var mail = new MimeMessage(); //create a new empty mail 
+
+            //Add addresses for the mail
+            mail.From.Add(new MailboxAddress("Vendor Application Inventory Platform", "JohnWilliamCitiSoft@gmail.com"));
 
             // Ensure that the 'to' parameter is a valid email address
-            if (MailboxAddress.TryParse(to, out var toAddress))
+            if (MailboxAddress.TryParse(toEmail, out var toAddress))
             {
-                message.To.Add(toAddress);
+                mail.To.Add(toAddress); //add the toEmail to the email to be sent to
             }
             else
             {
-                // Handle invalid email address (throw an exception, log an error, etc.)
-                throw new ArgumentException("Invalid 'to' email address", nameof(to));
+                // Handle invalid email address (throw an exception by loggin the error)
+                throw new ArgumentException("Invalid 'to' email address", nameof(toEmail));
             }
 
-            message.Subject = subject;
+            mail.Subject = subject; //Set the subject of the mail to the parameter 
 
-            var bodyBuilder = new BodyBuilder { HtmlBody = body };
-            message.Body = bodyBuilder.ToMessageBody();
+            //Create the email body and render the message to the body
+            var emailBody = new BodyBuilder { HtmlBody = messageToSend };
+
+
+            mail.Body = emailBody.ToMessageBody();
 
             // Send the email
-            _smtpClient.Send(message);
+            _smtpClient.Send(mail);
         }
 
          
         public void Disconnect()
         {
-            // Disconnect from the SMTP server
+            // Disconnect from the SMTP server and free up resource by deleting the smtpClient object
             _smtpClient.Disconnect(true);
             _smtpClient.Dispose();
         }
